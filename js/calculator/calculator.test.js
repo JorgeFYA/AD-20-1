051805e85e3393f95587d1c658c745026169f903
Francisco Yañez <jorgeyanezar@gmail.com>
2025-09-28 17:43:47 -0600

    task 1 to 3 completed

--- js/calculator/calculator.test.js
+++ js/calculator/calculator.test.js
@@ -15,3 +15,27 @@ test('subtract 5 - 4 to equal 1', () => {
 test('subtract -5 - -10 to equal 5', () => {
   expect(calculator.subtract(-5, -10)).toBe(5);
 });
+
+describe('testing functionality of "divide" function', () => {
+  test('divide 80 / 5 to equal 16', () => {
+    expect(calculator.divide(80, 5)).toBe(16);
+  });
+
+  test('divide -5 / 5 to equal -1', () => {
+    expect(calculator.divide(-5, 5)).toBe(-1);
+  });
+
+  test('should handle division by zero', () => {
+    expect(calculator.divide(10, 0)).toBe("Can't use zero as second parameter");
+  });
+});
+
+describe('testing functionality of "multiply" function', () => {
+  test('multiply 80 * -5 to equal -400', () => {
+    expect(calculator.multiply(80, -5)).toBe(-400);
+  });
+
+  test('multiply -5 * -5 to equal 25', () => {
+    expect(calculator.multiply(-5, -5)).toBe(25);
+  });
+});
--- js/user/user-controller.test.js
+++ js/user/user-controller.test.js
@@ -1,18 +1,39 @@
-const UserController = require("./user-controller");
-const User = require("./user");
+const UserController = require('./user-controller');
+const User = require('./user');
 
 const userController = new UserController();
 
-test('add user to userController', () => {    
-    let user = new User(1234,"Santiago", "santiago@generation.org");
-    userController.add(user);    
-    expect(userController.getUsers()).toContain(user);
-  });
+test('add user to userController', () => {
+  let user = new User(1234, 'Santiago', 'santiago@generation.org');
+  userController.add(user);
+  expect(userController.getUsers()).toContain(user);
+});
 
-test('remove user to userController', () => {    
-    let user = new User(1234,"Santiago", "santiago@generation.org");
-    userController.add(user);    
-    userController.remove(user);
-    expect(userController.users).not.toContain(user);
-  });
+test('remove user to userController', () => {
+  let user = new User(1234, 'Santiago', 'santiago@generation.org');
+  userController.add(user);
+  userController.remove(user);
+  expect(userController.users).not.toContain(user);
+});
 
+describe('testing "findByEmail" function', () => {
+  test('finding an existing user', () => {
+    const paco = new User(73, 'Paco', 'paco@gmail.com');
+    userController.add(paco);
+    expect(userController.findByEmail('paco@gmail.com')).toBe(paco);
+  });
+  test('looking for a non-existing email', () => {
+    expect(userController.findByEmail('paco2@gmail.com')).toBeUndefined();
+  });
+});
+
+describe('testing "findById" function', () => {
+  test('finding an existing user', () => {
+    const john = new User(104, 'john', 'doe@email.com');
+    userController.add(john);
+    expect(userController.findById(104)).toBe(john);
+  });
+  test('looking for a non-existing user', () => {
+    expect(userController.findById(74)).toBeUndefined();
+  });
+});
